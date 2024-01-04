e64e0798221316a1b737e3a830393ef4193b7230
sajjad saleh <sajjad.saleh2004@gmail.com>
2024-01-04 04:14:37 +0330

    part #4

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ function main() {
         );
     });
 
+
+makeTodoElelment(JSON.parse( localStorage.getItem("todos")));
+
     //Add Todo In LocalStorage
     addBtn.addEventListener("click", () => {
         const item = todoInput.value.trim();
@@ -31,9 +34,59 @@ function main() {
             }
 
             todos.push(currentTodo);
-            localStorage.setItem("todos",JSON.stringify(todos));
+            localStorage.setItem("todos", JSON.stringify(todos));
         }
-    })
+    });
+}
 
+
+function makeTodoElelment(todoArray) {
+    if (!todoArray) {
+        return null;
+    }
+    todoArray.forEach((todoObject) => {
+        //create Html Elements of Todo
+        const card = document.createElement("li");
+        const cbContainer = document.createElement("div");
+        const cbInput = document.createElement("input");
+        const checkSpan = document.createElement("span");
+        const item = document.createElement("p");
+        const clearBtn = document.createElement("button");
+        const img = document.createElement("img");
+
+
+        //َAdd classes
+        card.classList.add("card");
+        cbContainer.classList.add("cb-container");
+        cbInput.classList.add("cb-input");
+        checkSpan.classList.add("check");
+        item.classList.add("item");
+        clearBtn.classList.add("clear");
+
+        //Add Attributes
+        card.setAttribute("draggable",true);
+        cbInput.setAttribute("type","checkbox");
+        img.setAttribute("src","assets/images/icon-cross.svg");
+        img.setAttribute("alt","Clear it");
+        item.textContent= todoObject.item;
+        //َAdd Eventlistener
+
+
+        //Set Element by Parent child
+        clearBtn.appendChild(img);
+        cbContainer.appendChild(cbInput);
+        cbContainer.appendChild(checkSpan);
+        card.appendChild(cbContainer);
+        card.appendChild(item);
+        card.appendChild(clearBtn);
+
+
+        document.querySelector(".todos").appendChild(card);
+
+
+        
+
+
+    });
 }
 document.addEventListener('DOMContentLoaded', main);
